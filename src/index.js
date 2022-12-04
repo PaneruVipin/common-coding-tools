5c20cf244aee476cb36c0255f0be620884758518
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-12-04 22:50:59 +0530

    miner

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-const {search} = require("./array");
-const  page2= require("./page2");
-const  page3 = require("./page3");
+const { search, descending, ascending } = require("./array");
 
 module.exports = {
   search,
-  page2,
-  page3,
+  descending,
+  ascending,
 };
