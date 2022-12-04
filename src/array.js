6323419504717d8dab0c97119950fc89aba5873f
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-12-04 16:59:15 +0530

    node modules delete

--- src/array.js
+++ src/array.js
@@ -42,12 +42,3 @@ module.exports = {
   search,
 };
 
-/**
- * echo "# common-coding-tools" >> README.md
-git init
-git add -A
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/PaneruVipin/common-coding-tools.git
-git push -u origin main
- */
