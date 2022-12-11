207c19ff7d7d1d2477c2c508a1f81f6831a910c3
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-12-11 14:09:41 +0530

    string page

---
+++ src/string.js
@@ -0,0 +1,35 @@
+const lowerCase = () => {
+  const firstCharacter = (str) => {
+    const subStrs = str.split(" ");
+    const lowerCase = subStrs
+      .map((subStr) => {
+        return subStr.charAt(0).toLowerCase() + subStr.slice(1);
+      })
+      .join(" ");
+    return lowerCase;
+  };
+  const all = (str) => {
+    const newStr = str.toLowerCase();
+    return newStr;
+  };
+  return { firstCharacter, all };
+};
+
+const upperCase = () => {
+  const firstCharacter = (str) => {
+    const subStrs = str.split(" ");
+    const upperCase = subStrs
+      .map((subStr) => {
+        return subStr.charAt(0).toUpperCase() + subStr.slice(1);
+      })
+      .join(" ");
+    return upperCase;
+  };
+  const all = () => {
+    const newStr = str.toUpperCase();
+    return newStr;
+  };
+  return { firstCharacter, all };
+};
+
+module.exports = { lowerCase, upperCase };
