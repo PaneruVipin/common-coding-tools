610fe50955b3bd418a3992b2afef178fe6e4d7fc
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-12-24 00:29:15 +0530

    miner

--- src/helper/validate/rule.js
+++ src/helper/validate/rule.js
@@ -1,6 +1,57 @@
 const { upperCase, lowerCase } = require("../../string");
 const { isObject, isArray } = require("../../types");
-const { validation } = require("../../validation");
+var validation = (body, rules) => {
+  const data = {};
+  const errors = [];
+  const ruleKeys = Object.keys(rules);
+  ruleKeys?.forEach((key, i) => {
+    let schemaList;
+    if (rules?.[key]?.includes("{") || rules?.[key]?.includes("["))
+      schemaList = [rules?.[key]];
+    else {
+      schemaList = rules?.[key].split("|").map((str) => str?.trim());
+    }
+    let returnedValue;
+    // console.log("schemaList"schemaList)
+    schemaList?.forEach((schema, i) => {
+      const rule = schema?.split(":")?.[0]?.trim();
+      const ruleValue = schema
+        ?.split(":")
+        .filter((s, i) => i)
+        .join(":");
+      // console.log(rule,ruleValue)
+      if (i) {
+        if (schemaList.includes("trim")) {
+          const newData = ruleHandlers?.["trim"](
+            returnedValue || body?.[key],
+            key,
+            ruleValue
+          );
+          if (newData?.error) errors.push(newData?.error);
+          returnedValue = newData?.value;
+        }
+        const newData = ruleHandlers?.[rule](
+          returnedValue || body?.[key],
+          key,
+          ruleValue
+        );
+        if (newData?.error) errors.push(newData?.error);
+        returnedValue = newData?.value;
+      } else {
+        // console.log(body?.[key], key, ruleValue)
+        const newData = ruleHandlers?.[rule](body?.[key], key, ruleValue);
+        if (newData?.error) errors.push(newData?.error);
+        returnedValue = newData?.value;
+      }
+    });
+    data[key] = returnedValue;
+  });
+  if (!errors.length) {
+    return { data };
+  } else {
+    return { errors };
+  }
+};
 
 const required = (value, feild) => {
   const data = {};
@@ -327,7 +378,7 @@ const removeAllWhiteSpace = (value) => {
   return data;
 };
 
-const ruleHandlers = {
+var ruleHandlers = {
   defaultValue,
   removeAllWhiteSpace,
   boolean,
