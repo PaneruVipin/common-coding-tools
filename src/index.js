60a6e20f9d42ec049a1440ac9f0b83f175933b76
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-12-30 22:47:10 +0530

    2.0.5

--- src/index.js
+++ src/index.js
@@ -1,44 +1,14 @@
-const { makeHash, verifyHash } = require("./hash");
-const { randomNumber, randomData, randomString } = require("./random");
-const { lowerCase, upperCase } = require("./string");
-const random = { randomNumber, randomData, randomString };
-const {
-  type,
-  isUrl,
-  isEmail,
-  isBoolean,
-  isArray,
-  isObject,
-  isNumber,
-  isString,
-} = require("./types");
-const { validation, schema } = require("./validation");
-const {
-  search,
-  descending,
-  ascending,
-  arrayToObject,
-  objectToArray,
-} = require("./_array");
-const hashing = { makeHash, verifyHash };
-const array = { search, descending, ascending, arrayToObject, objectToArray };
-const string = { lowerCase, upperCase };
-const types = {
-  type,
-  isUrl,
-  isEmail,
-  isBoolean,
-  isArray,
-  isObject,
-  isNumber,
-  isString,
-};
-
+const hashing = require("./hash");
+const random = require("./random");
+const string = require("./string");
+const validation = require("./validation");
+const types = require("./types");
+const array = require("./_array");
 module.exports = {
   array,
   string,
   types,
-  validation: { validation, schema },
+  validation,
   hashing,
   random,
 };
