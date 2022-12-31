e3862dac376cc9a6ff39fc1b239bb60a66e6c937
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-12-31 18:03:50 +0530

    m

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const hashing = require("../hashing");
 const random = require("../random");
 const string = require("../string");
-const validation = require("../types/validation");
+const validation = require("../validation");
 const types = require("../types");
 const array = require("../array");
 
