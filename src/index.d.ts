82d545eb813f857f0dbbda6cfb6c20da727a92e1
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-12-31 16:27:53 +0530

    types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,7 @@
-export declare module commonCodingTools{
-    export * as array from "../array";
-    export * as hashing from "../hashing";
-    export * as random from "../random";
-    export * as string from "../string";
-    export * as types from "../types";
-    export * as validation from "../validation";
-    export * as string from "../string";
-}
+export * as array from "../array";
+export * as hashing from "../hashing";
+export * as random from "../random";
+export * as string from "../string";
+export * as types from "../types";
+export * as validation from "../validation";
+export * as string from "../string";
